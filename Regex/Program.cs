e80aa22e2f3b7a1520bf547aec50886e973f82a3
Arka <arka.spectacular@gmail.com>
2021-08-11 17:07:25 +0530

    [Arka]Added:Name Validation

---
+++ Regex/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RegexValidation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regex Validation!");
+            Validation validation = new Validation();
+            Console.WriteLine(validation.validateName("Arka"));
+        }
+    }
+}
---
+++ Regex/Validation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace RegexValidation
+{
+    public class Validation
+    {
+        public string nameValidate = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public bool validateName(string name)
+        {
+            return Regex.IsMatch(name, nameValidate);
+        }
+    }
+}
