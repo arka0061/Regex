0376097efc4b01b0fac7c0963139e2ef8541ac41
Arka <arka.spectacular@gmail.com>
2021-08-11 17:12:18 +0530

    [Arka]Refactor:User inputs name

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -8,7 +8,12 @@ namespace RegexValidation
         {
             Console.WriteLine("Welcome to Regex Validation!");
             Validation validation = new Validation();
-            Console.WriteLine(validation.validateName("Arka"));
+            Console.WriteLine("Enter a Name :");
+            string name = Console.ReadLine();
+            if (validation.validateName(name) == true)
+                Console.WriteLine("Username :"+name);
+            else
+                Console.WriteLine("Enter name in correct format!");
         }
     }
 }
